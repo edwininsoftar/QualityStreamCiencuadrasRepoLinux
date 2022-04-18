3a091ec75d5af0b0436d21b20535a90a5a0a43ad
Insoftar <leonardo.carreno@insoftar.com>
2022-04-18 17:28:33 -0500

    create funtion constructora

--- src/test/java/contactStream/contactPage.java
+++ src/test/java/contactStream/contactPage.java
@@ -9,6 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -47,22 +48,28 @@ public class contactPage extends BasicWrap{
 	By locator_buttonSend = By.xpath("//*[@id=\"contactSalesForm\"]/div[7]/button[1]");
 	By locator_x = By.xpath("//*[@id=\"detail-contact-sales\"]/button/span/span");
 	By locator_enterButton = By.linkText("INGRESAR");
-	By locator_user = By.id("mat-input-0");
-	By locator_password = By.id("mat-input-1");
+	By locator_user = By.cssSelector("input[formcontrolname=\"username\"]");
+	By locator_password = By.cssSelector("input[formcontrolname=\"password\"]");
 	By locator_logIn = By.xpath("/html/body/app-root/div/app-login/div/lib-cc-login/div/div/div[3]/form/button/span");
-	By locator_menu = By.xpath("//*[@id=\"headerHome\"]/lib-cc-header/header/nav[2]/button[1]/div/a/div");
+	By locator_menu = By.cssSelector("div[class=\"circle-name\"]");
 	By locator_myReports = By.linkText("Mis Reportes");
+	By locator_startDate = By.id("fechaDesde");
+	By locator_endDate = By.id("fechaHasta");
+	By locator_Consult = By.id("btn-submit-home");
+	By locator_clickForm = By.id("//*[@id=\"tablaReportes\"]/table/tbody/tr[2]/td[5]");
 	
 	String Contact = "Contactar1"; //Contactar1,Contactar2,Contactar3,Contactar4,
-	String Constructora = "Constructora tu hogar";//nombre de la constructora que va a buscar
+	String Constructora = "Constructora uno";//nombre de la constructora que va a buscar
 	String name = "fabian corrales"; // Nombre o razon social
 	String email = "leonardo2121@yopmail.com";
 	String cellPhone = "3123454345";
 	String customerSupport = "lunes a viernes am"; //elige un horario para contactarme lunes a viernes am,lunes a viernes pm,Fin de semana am,Fin de semana pm 
 	String livinPlace = "Para invertir"; //para que buscas vivienda Para invertir, Para vivir
     String planToBuy = "3 meses"; // cuando planeas comprar 3 meses, 6 meses, 9 meses,
-    String user = "qa.constructorahogar21@yopmail.com";
-    String password = "LeonO1O1*";
+    String user = "qa.constructorahogar21@yopmail.com";//usuario de constructora
+    String password = "LeonO1O1*";//contraseña
+    String startDate = "2022-04-01"; //fecha inicio de reporte
+    String endDate = "2022-04-30";// fecha de fin de reporte
     
 	public contactPage(WebDriver driver) {
 		super(driver);
@@ -148,11 +155,11 @@ public class contactPage extends BasicWrap{
 			ewait.until(ExpectedConditions.elementToBeClickable(locator_captcha)).click();
 			driver.switchTo().defaultContent();
 			
-			click(locator_buttonSend);
+            click(locator_buttonSend);
 			Thread.sleep(8000);
 			click(locator_x);
 		
-		} catch (NoSuchElementException e) {
+		} catch (NoSuchElementException e){ 
 			System.out.println("No se encuentra el elemento: " + e);
 		} catch (TimeoutException e) {
 			System.out.println("Error de tiempo de espera para ejecutar el comando: " + e);
@@ -166,8 +173,34 @@ public class contactPage extends BasicWrap{
 	}
 	public void userBuilder() throws InterruptedException {
 		
+		try {
 		Thread.sleep(8000);
 		click(locator_enterButton);
+        type(user, locator_user);
+        type(password, locator_password);
+        click(locator_logIn);
+        Thread.sleep(8000);
+        click(locator_menu);
+        click(locator_myReports);
+        WebElement dta = driver.findElement(locator_startDate);
+        dta.clear();
+		dta.sendKeys(startDate);
+        WebElement dtaFin = driver.findElement(locator_endDate);
+        dta.clear();
+		dta.sendKeys(endDate);
+		String numText = driver.findElement(locator_clickForm).getText();
+		System.out.println(numText);
+		
+		}catch(NoSuchElementException e){
+			System.out.println("No se encuentra el elemento: " + e);
+		}catch(TimeoutException e){
+			System.out.println("Error de tiempo de espera para ejecutar el comando: " + e);
+		}catch(Exception e){
+			System.out.println("Error" + e);
+		}finally {
+			System.out.println("Fin validaciones del contactar");
+		}
+		
 		
 	}
 
--- src/test/java/contactStream/contact_test.java
+++ src/test/java/contactStream/contact_test.java
@@ -17,7 +17,7 @@ public class contact_test {
 	public void setUp() throws Exception {
 		ContactPage = new contactPage(driver);
 		driver = ContactPage.chromeDriverConnection();
-		ContactPage.visit("https://qa.ciencuadras.com/");
+		ContactPage.visit("https://dev.ciencuadras.com/");
 	}
 
 	@After
@@ -28,7 +28,7 @@ public class contact_test {
 	public void test() throws InterruptedException {
 		ContactPage.contactBuyNew();
 		ContactPage.formContact();
-		ContactPage.userBuilder();
+		//ContactPage.userBuilder();
 		
 	}
 

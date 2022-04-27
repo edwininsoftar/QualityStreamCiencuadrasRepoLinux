161344126eb7d1a706f53e023f4d90b74049568e
Edwin Pulido <edwin.pulido@insoftar.com>
2022-04-27 11:03:33 -0500

    Función publicacion de un proyecto

--- src/test/java/CreatePage/CreateProjectPageConstructora.java
+++ src/test/java/CreatePage/CreateProjectPageConstructora.java
@@ -1,5 +1,7 @@
 package CreatePage;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
@@ -33,23 +35,25 @@ public class CreateProjectPageConstructora extends BasicWrap{
 	By locator_date = By.id("input_fecha_entrega");
 	By locator_condition = By.id("select_estado");
 	By locator_pay = By.id("select_membresia");
+	By locator_directionComplit = By.id("publicador_direccion");
 	
 	//Login
-	String username = "ciencuadrasconstructora12@yopmail.com";//usuario constructora
+	String username = "qa.constructorahogar21@yopmail.com";//usuario constructora qa.constructorahogar21@yopmail.com //ciencuadrasconstructora12@yopmail.com
 	String password = "LeonO1O1*"; // contraseña
 	//Crear nuevo proyecto
 	String nameProject = "Oasis"; // nombre del proyecto
 	String stage = "Sobre Plano"; //etapa del proyecto puede ser "seleccione, Sobre Plano, Construcion, Entrega Inmediata"
-	String projectPhase = "";//Ingrese Venta, PreVenta, Oferta Terminada, Lanzamiento
+	String projectPhase = "Venta";//Ingrese Venta, PreVenta, Oferta Terminada, Lanzamiento
 	String straum = "3";//Ingrese 1,2,3,4,5 o 6 
 	String department = "Cundinamarca";// Ingrese el departamento
 	String city = "Bogotá";//ingrese la ciudad
-	String location = "Ciudad Bolivar";// Ingrese la localidad
+	String location = "Ciudad Bolívar";// Ingrese el volor de la localidad que decea segun las opciones html
 	String neighborhood = "Madelena";//Ingrese el barrio 
 	String direction = "Si";//Ingrese Si y No
 	String date = "06062022";// Ingrese la fecha 
 	String condition = "Activo";//Ingresar Activo, Inactivo, Duplicado 
-	String pay = "Pago";//Ingrese Pago, Gratis
+	String pay = "Gratis";//Ingrese Pago, Gratis
+	String directionComplit = "Calle 60 sur#66-63";
 	
 	public CreateProjectPageConstructora(WebDriver driver) {
 		super(driver);
@@ -85,6 +89,45 @@ public class CreateProjectPageConstructora extends BasicWrap{
 			WebElement eta = driver.findElement(locator_stage);
 			Select sel = new Select(eta);
 			sel.selectByVisibleText(stage);
+			Thread.sleep(3000);
+			WebElement fas = driver.findElement(locator_projectPhase);
+			Select sel2 = new Select(fas);
+			sel2.selectByVisibleText(projectPhase);
+			Thread.sleep(3000);
+			WebElement est = driver.findElement(locator_stratum);
+			Select sel3 = new Select(est);
+			sel3.selectByVisibleText(straum);
+			Thread.sleep(3000);
+			WebElement dep = driver.findElement(locator_department);
+			Select sel4 = new Select(dep);
+			sel4.selectByVisibleText(department);
+			Thread.sleep(3000);
+			WebElement ciu = driver.findElement(locator_city);
+			Select sel5 = new Select(ciu);
+			sel5.selectByVisibleText(city);
+			Thread.sleep(3000);
+			WebElement loc = driver.findElement(locator_location);
+			Select sel6 = new Select(loc);
+			sel6.selectByVisibleText(location);
+			Thread.sleep(3000);
+			WebElement bar = driver.findElement(locator_neighborhood);
+			Select sel7 = new Select(bar);
+			sel7.selectByVisibleText(neighborhood);
+			Thread.sleep(3000);
+			WebElement dir = driver.findElement(locator_direction);
+			Select sel8 = new Select(dir);
+			sel8.selectByVisibleText(direction);
+			Thread.sleep(3000);
+			type(date, locator_date);
+			Thread.sleep(3000);
+			WebElement con = driver.findElement(locator_condition);
+			Select sel9 = new Select(con);
+			sel9.selectByVisibleText(condition);
+			Thread.sleep(3000);
+			WebElement mem = driver.findElement(locator_pay);
+			Select sel10 = new Select(mem);
+			sel10.selectByVisibleText(pay);
+			type(directionComplit, locator_directionComplit);
 		} catch (NoSuchElementException e) {
 			System.out.println("Error: " + e);
 		}catch (TimeoutException e) {
